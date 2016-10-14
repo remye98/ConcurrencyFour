90e662d9003dbb12239c64f545b05ce1cae66db1
Remy <Remye98@gmail.com>
2016-10-14 12:18:43 +0200

    Initial commit

---
+++ SectionAgent.java
@@ -0,0 +1,15 @@
+import akka.actor.UntypedActor;
+
+/**
+ * Created by Remy on 14-10-2016.
+ */
+public class SectionAgent extends UntypedActor{
+
+    //bruh
+    // TODO: 14-10-2016 bruh 
+
+    @Override
+    public void onReceive(Object o) throws Throwable {
+
+    }
+}
